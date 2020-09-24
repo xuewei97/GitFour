65a80093ee45943271dc09a3afa82afaea203606
xuewei97 <279838169@qq.com>
2020-09-24 16:22:20 +0800

    1

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -4,10 +4,9 @@ package com;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("1");
+	}
 }
